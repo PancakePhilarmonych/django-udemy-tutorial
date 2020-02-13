ec422f830fc1779d02a4798f9f490d6701160403
Aleksandr Panfilov <rikko.alexandr@gmail.com>
2020-02-14 00:59:37 +0300

    set up admin panel fields for Listings and Realtors

--- realtors/models.py
+++ realtors/models.py
@@ -8,7 +8,7 @@ class Realtor(models.Model):
 	phone = models.CharField(max_length=20)
 	email = models.CharField(max_length=50)
 	is_mvp = models.BooleanField(default=False)
-	hire_data = models.DateTimeField(default=datetime.now, blank=True)
+	hire_date = models.DateTimeField(default=datetime.now, blank=True)
 
 	class Meta:
 		verbose_name = "Realtor"
