8446b444924f789d131d3ec09737891fc2dbe721
Aleksandr Panfilov <rikko.alexandr@gmail.com>
2020-02-15 13:56:54 +0300

    add data from database to home and about pages

--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from listings.models import Listing
+from realtors.models import Realtor
+
 def index(request):
-	return render(request, 'pages/index.html')
+	# Get last listings
+	listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
+
+	contex = {
+		'listings': listings
+	}
+	print(listings)
+
+
+	return render(request, 'pages/index.html', contex)
 
 def about(request):
-	return render(request, 'pages/about.html')
+	# Get all realtors
+	realtors = Realtor.objects.order_by('-hire_date')
+
+	# Get MPV
+
+	mvp_realtors = Realtor.objects.all().filter(is_mvp=True)
+
+	context = {
+		'realtors' : realtors,
+		'mvp_realtors' : mvp_realtors
+	}
+
+	return render(request, 'pages/about.html', context)
