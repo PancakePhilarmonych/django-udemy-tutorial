4358c7dfb27d8cee693c81a43ea6fff4146e049c
Aleksandr Panfilov <rikko.alexandr@gmail.com>
2020-03-19 23:15:41 +0300

    delete email settings

--- btre/settings.py
+++ btre/settings.py
@@ -148,12 +148,12 @@ MESSAGE_TAGS = {
 
 # Email config
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'mail'
-EMAIL_HOST_PASSWORD = 'password'
-EMAIL_USE_TLS=True
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 587
+# EMAIL_HOST_USER = 'mail'
+# EMAIL_HOST_PASSWORD = 'password'
+# EMAIL_USE_TLS=True
 
 try:
     from .local_settings import *
